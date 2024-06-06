80d2994e89b82875f46f14544c4e94b43873b611
Md. Zahidul Islam Mehedi <87989202+Mohammed-ZIMS@users.noreply.github.com>
2024-06-06 03:57:51 +0100

    Add files via upload

---
+++ DataBases.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class DataBases {
+    Connection connection;
+    Statement statement;
+
+    String user = "root";
+    String url = "jdbc:mysql://localhost:3306/e_billing_system";
+    String password = "123456789";
+
+    public DataBases() {
+        try {
+            // Load MySQL JDBC Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Establish connection
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
